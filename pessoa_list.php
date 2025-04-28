90881d6ebecdcbd8b267ca226927a377f58e9f76
Ercilia Maria Coelho de Carvalho <erciliacarvalho2@gmail.com>
2025-04-28 11:44:27 -0300

    Ajustando a função delete

--- pessoa_list.php
+++ pessoa_list.php
@@ -24,6 +24,13 @@
             <?php
             $conn = new mysqli('localhost', 'root', '', 'cadastro_pessoa', 3306);
 
+            if( !empty($_GET['action']) and ($_GET['action'] == 'delete'))
+            {
+                $id = (int) $_GET['id'];
+
+                $conn->query("DELETE FROM pessoas WHERE id='{$id}'");
+            }
+            
             $result = $conn->query('SELECT * FROM pessoas ORDER BY id');
 
             while ($row = $result->fetch_assoc())
@@ -37,10 +44,10 @@
                 $id_cidade = $row['id_cidade'];
 
                 print '<tr>';
-                print "<td><a href='pessoa_form_edit.php?id={$id}'>
+                print "<td><a href='pessoa_form.php?action=edit&id={$id}'>
                 <img src='./img/edit.jpg'>
                 </a></td>";
-                print "<td><a href='pessoa_delete.php?id={$id}'>
+                print "<td><a href='pessoa_list.php?action=delete&id={$id}'>
                 <img src='./img/remove.jpg'>
                 </a></td>";
                 print "<td>{$id}</td>";
@@ -49,13 +56,13 @@
                 print "<td>{$bairro}</td>";
                 print "<td>{$telefone}</td>";
                 print "<td>{$email}</td>";
-                print '<tr>';
+                print '</tr>';
             }
             ?>
         </tbody>
     </table>
 
-    <button onclick="window.location='pessoa_form_insert.php'">
+    <button onclick="window.location='pessoa_form.php'">
         <img src="./img/insert.jpg" alt="inserção">
     </button>
 
