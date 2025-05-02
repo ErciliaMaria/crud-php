23d5047f14ce20358cab08a1a598b699a5b2f5ef
Ercilia Maria Coelho de Carvalho <erciliacarvalho2@gmail.com>
2025-05-02 15:31:08 -0300

    separando html de php

--- pessoa_form.php
+++ pessoa_form.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
-    <link href="css/form.css" rel="stylesheet" type="text/css" media="screen">
-</head>
-
-<body>
-    <?php
-    $id = '';
-    $nome = '';
-    $endereco = '';
-    $bairro = '';
-    $telefone = '';
-    $email = '';
-    $id_cidade = '';
+<?php
+    $pessoa = [];
+    $pessoa['id']        = '';
+    $pessoa['nome']      = '';
+    $pessoa['endereco']  = '';
+    $pessoa['bairro']    = '';
+    $pessoa['telefone']  = '';
+    $pessoa['email']     = '';
+    $pessoa['id_cidade'] = '';
 
     if (!empty($_REQUEST['action'])) {
         $conn = new mysqli('localhost', 'root', '', 'cadastro_pessoa', 3306);
@@ -26,24 +16,11 @@
                 $id = (int) $_GET['id'];
                 $result = $conn->query("SELECT * FROM pessoas WHERE id='{$id}'");
 
-                $row = $result->fetch_assoc();
-
-                $id = $row['id'];
-                $nome = $row['nome'];
-                $endereco = $row['endereco'];
-                $bairro = $row['bairro'];
-                $telefone = $row['telefone'];
-                $email = $row['email'];
-                $id_cidade = $row['id_cidade'];
+                $pessoa = $result->fetch_assoc();
             }
         } else if ($_REQUEST['action'] === 'save') {
             $id = $_POST['id'];
-            $nome = $_POST['nome'];
-            $endereco = $_POST['endereco'];
-            $bairro = $_POST['bairro'];
-            $telefone = $_POST['telefone'];
-            $email = $_POST['email'];
-            $id_cidade = $_POST['id_cidade'];
+            $pessoa = $_POST;
 
             if (empty($_POST['id'])) {
                 $dados = $_POST;
@@ -54,8 +31,12 @@
                 $next = (int) $row['next'] + 1;
 
                 $sql = "INSERT INTO pessoas(id, nome, endereco, bairro, telefone, email,id_cidade)
-VALUES ('{$next}',  '{$dados['nome']}', '{$dados['endereco']}', '{$dados['bairro']}', '{$dados['telefone']}', '{$dados['email']}', '{$dados['id_cidade']}')";
-
+                VALUES ('{$next}',  '{$pessoa['nome']}', 
+                                    '{$pessoa['endereco']}', 
+                                    '{$pessoa['bairro']}', 
+                                    '{$pessoa['telefone']}',
+                                    '{$pessoa['email']}', 
+                                    '{$pessoa['id_cidade']}')";
                 $result = mysqli_query($conn, $sql);
 
                 if ($result) {
@@ -69,13 +50,13 @@ VALUES ('{$next}',  '{$dados['nome']}', '{$dados['endereco']}', '{$dados['bairro
 
                 $conn = new mysqli('localhost', 'root', '', 'cadastro_pessoa', 3306);
 
-                $sql = "UPDATE pessoas SET nome     = '{$dados['nome']}',
-                               endereco = '{$dados['endereco']}',
-                               bairro   = '{$dados['bairro']}',
-                               telefone = '{$dados['telefone']}',
-                               email    = '{$dados['email']}',
-                              id_cidade = '{$dados['id_cidade']}'
-                           WHERE id = '{$dados['id']}'
+                $sql = "UPDATE pessoas SET nome     = '{$pessoa['nome']}',
+                               endereco = '{$pessoa['endereco']}',
+                               bairro   = '{$pessoa['bairro']}',
+                               telefone = '{$pessoa['telefone']}',
+                               email    = '{$pessoa['email']}',
+                              id_cidade = '{$pessoa['id_cidade']}'
+                           WHERE id = '{$pessoa['id']}'
                         ";
                 $result = $conn->query($sql);
 
@@ -85,36 +66,18 @@ VALUES ('{$next}',  '{$dados['nome']}', '{$dados['endereco']}', '{$dados['bairro
             }
         }
     }
-    ?>
-    <form enctype="multipart/form-data" method="post" action="pessoa_form.php?action=save">
-        <label for="">Código</label>
-        <input name="id" readonly="1" type="text" value="<?= $id ?>">
+    require_once 'lista_combo_cidades.php';
+    $cidades = lista_combo_cidades($pessoa['id_cidade']);
 
-        <label for="">Nome</label>
-        <input name="nome" type="text" value="<?= $nome ?>">
-
-        <label for="">Endereço</label>
-        <input name="endereco" type="text" value="<?= $endereco ?>">
-
-        <label for="">Bairro</label>
-        <input name="bairro" type="text" value="<?= $bairro ?>">
-
-        <label for="">Telefone</label>
-        <input name="telefone" type="text" value="<?= $telefone ?>">
-
-        <label for="">Email</label>
-        <input name="email" type="text" value="<?= $email ?>">
-
-        <label for="">Cidade</label>
-        <select name="id_cidade">
-            <?php
-            require_once 'lista_combo_cidades.php';
-            print lista_combo_cidades($id_cidade);
-            ?>
-        </select>
-
-        <input type="submit">
-    </form>
-</body>
-
-</html>
+    $form = file_get_contents('html/form.html');
+    $form = str_replace('{id}', $pessoa['id'], $form);
+    $form = str_replace('{nome}', $pessoa['nome'], $form);
+    $form = str_replace('{endereco}', $pessoa['endereco'], $form);
+    $form = str_replace('{bairro}', $pessoa['bairro'], $form);
+    $form = str_replace('{telefone}', $pessoa['telefone'], $form);
+    $form = str_replace('{email}', $pessoa['email'], $form);
+    $form = str_replace('{id_cidade}', $pessoa['id_cidade'], $form);
+    $form = str_replace('{cidades}', $cidades, $form);
+    
+    print $form;
+?>
--- pessoa_list.php
+++ pessoa_list.php
@@ -1,70 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de pessoas</title>
-    <link href="css/list.css" rel="stylesheet" type="text/css" media="screen">
-</head>
-<body>
-    <table border=1>
-        <thead>
-            <tr>
-                <td></td>
-                <td></td>
-                <td>Id</td>
-                <td>Nome</td>
-                <td>Endereço</td>
-                <td>Bairro</td>
-                <td>Telefone</td>
-                <td>Email</td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $conn = new mysqli('localhost', 'root', '', 'cadastro_pessoa', 3306);
+<?php
+$conn = new mysqli('localhost', 'root', '', 'cadastro_pessoa', 3306);
 
-            if( !empty($_GET['action']) and ($_GET['action'] == 'delete'))
-            {
-                $id = (int) $_GET['id'];
+if( !empty($_GET['action']) and ($_GET['action'] == 'delete'))
+{
+    $id = (int) $_GET['id'];
 
-                $conn->query("DELETE FROM pessoas WHERE id='{$id}'");
-            }
+    $conn->query("DELETE FROM pessoas WHERE id='{$id}'");
+}
             
-            $result = $conn->query('SELECT * FROM pessoas ORDER BY id');
+$result = $conn->query('SELECT * FROM pessoas ORDER BY id');
 
-            while ($row = $result->fetch_assoc())
-            {
-                $id = $row['id'];
-                $nome = $row['nome'];
-                $endereco = $row['endereco'];
-                $bairro = $row['bairro'];
-                $telefone = $row['telefone'];
-                $email = $row['email'];
-                $id_cidade = $row['id_cidade'];
+$items = '';
+while ($row = $result->fetch_assoc())
+{
+    $item = file_get_contents('html/item.html');
+    $item = str_replace('{id}', $row['id'], $item);
+    $item = str_replace('{nome}', $row['nome'], $item);
+    $item = str_replace('{endereco}', $row['endereco'], $item);
+    $item = str_replace('{bairro}', $row['bairro'], $item);
+    $item = str_replace('{telefone}', $row['telefone'], $item);
+    $item = str_replace('{email}', $row['email'], $item);
 
-                print '<tr>';
-                print "<td><a href='pessoa_form.php?action=edit&id={$id}'>
-                <img src='./img/edit.jpg'>
-                </a></td>";
-                print "<td><a href='pessoa_list.php?action=delete&id={$id}'>
-                <img src='./img/remove.jpg'>
-                </a></td>";
-                print "<td>{$id}</td>";
-                print "<td>{$nome}</td>";
-                print "<td>{$endereco}</td>";
-                print "<td>{$bairro}</td>";
-                print "<td>{$telefone}</td>";
-                print "<td>{$email}</td>";
-                print '</tr>';
-            }
-            ?>
-        </tbody>
-    </table>
+    $items .= $item;
+}
 
-    <button onclick="window.location='pessoa_form.php'">
-        <img src="./img/insert.jpg" alt="inserção">
-    </button>
+$list = file_get_contents('html/list.html');
+$list = str_replace('{items}', $items, $list);
 
-</body>
-</html>
+print $list;
+?>
